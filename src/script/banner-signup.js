4d895fbf281bd94455f4f1fc1b8ff8a4c7c7120f
Reinhold Seiss <reinhold.seiss@student.tugraz.at>
2016-11-28 02:18:33 +0100

    Compressed images

---
+++ src/script/banner-signup.js
@@ -0,0 +1,27 @@
+(function() {
+
+
+  function adjustSignupBanner() {
+    var w = $(window).width();
+    if (w > 1920 && w < 2651)
+      $('#banner-signup').css('background-image', 'url("images/banner-signup-background-2650-min.png")');
+    else if (w > 2650)
+      $('#banner-signup').css('background-image', 'url("images/banner-signup-background-5760.png")');
+    else
+      $('#banner-signup').css('background-image', 'url("images/banner-signup-background-1920-min.png")');
+  }
+
+  $(document).ready(function(){
+    adjustSignupBanner();
+
+    $(window).on('resize', function() {
+      adjustSignupBanner();
+    });
+
+    $('.btn-exit').on('click', function() {
+      $('#banner-signup').slideUp();
+    });
+  });
+
+
+})();
