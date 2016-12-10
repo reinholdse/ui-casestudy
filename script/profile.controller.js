2fd8b54e01631fb20c83eb20ef84924036366adc
pantos1 <piotrek.antosiuk@gmail.com>
2016-12-10 18:40:25 +0100

    Added photo expansion

--- script/profile.controller.js
+++ script/profile.controller.js
@@ -1,1 +1,38 @@
-(function(App))
+(function(App){
+'use strict';
+
+var ProfileController = {
+
+  settings:{
+    body: $('body'),
+    image: $('.image'),
+    gallery: $('.gallery'),
+    photoExpanded: $('#photo-expanded'),
+    back: $('.btn-back')
+},
+
+  init: function(){
+    ProfileController.bindUI();
+    ProfileController.settings.photoExpanded.css("display", "none");
+  },
+
+  bindUI: function(){
+    ProfileController.settings.image.on('click', function(e) {
+      e.preventDefault();
+      ProfileController.settings.gallery.css("display", "none");
+      var src = $(this).attr("src");
+      ProfileController.settings.photoExpanded.find('.graphic').attr("src", src);
+      ProfileController.settings.photoExpanded.css("display", "flex");
+    });
+    ProfileController.settings.back.on('click', function(e){
+      e.preventDefault();
+      ProfileController.settings.photoExpanded.css("display", "none");
+      ProfileController.settings.gallery.css("display", "initial");
+    });
+
+  }
+};
+
+App.settings.modules['profile.controller'] = ProfileController;
+
+})(App);
